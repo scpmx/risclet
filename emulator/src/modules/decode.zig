0ea47cc6f33b4a6c3946695acc507eca3c8ad29b
Stephen <13601394+scpmx@users.noreply.github.com>
2024-12-14 22:50:37 -0500

    Separate decode from execute

---
+++ emulator/src/modules/decode.zig
@@ -0,0 +1,373 @@
+const RawInstruction = @import("./instruction.zig").RawInstruction;
+const encode = @import("/encoder.zig");
+
+const RType = struct { rd: u5, rs1: u5, rs2: u5 };
+
+const IType = struct { rd: u5, rs1: u5, imm: i32 };
+
+const SType = struct { rs1: u5, rs2: u5, imm: i32 };
+
+const BType = struct { rs1: u5, rs2: u5, imm: i32 };
+
+const UType = struct { rd: u5, imm: i32 };
+
+const JType = struct { rd: u5, imm: i32 };
+
+const SysType = struct { rd: u5, rs1: u5, csr: u12 };
+
+const SysImmType = struct { rd: u5, rs1: u5, csr: u12, imm: u4 };
+
+pub const DecodedInstruction = union {
+    // R-Type Instructions
+    ADD: RType,
+    SUB: RType,
+    SLL: RType,
+    SLT: RType,
+    SLTU: RType,
+    XOR: RType,
+    SRL: RType,
+    SRA: RType,
+    OR: RType,
+    AND: RType,
+
+    // I-Type
+    ADDI: IType,
+    SLLI: IType,
+    SLTI: IType,
+    SLTIU: IType,
+    XORI: IType,
+    SRLI: IType,
+    ORI: IType,
+    ANDI: IType,
+    LB: IType,
+    LH: IType,
+    LW: IType,
+    LBU: IType,
+    LHU: IType,
+    JALR: IType,
+
+    // S-Type
+    SB: SType,
+    SH: SType,
+    SW: SType,
+
+    // B-Type
+    BEQ: BType,
+    BNE: BType,
+    BLT: BType,
+    BGE: BType,
+    BLTU: BType,
+    BGEU: BType,
+
+    // U-Type
+    LUI: UType,
+    AUIPC: UType,
+
+    // J-Type
+    JAL: JType,
+
+    // System
+    WFI: void,
+    SRET: void,
+    ECALL: void,
+    EBREAK: void,
+
+    CSRRW: SysType,
+    CSRRS: SysType,
+    CSRRC: SysType,
+    CSRRWI: SysImmType,
+    CSRRSI: SysImmType,
+    CSRRCI: SysImmType,
+
+    // Fence
+    FENCE: void,
+    FENCEI: void,
+};
+
+pub fn decode(instruction: RawInstruction) !DecodedInstruction {
+    switch (instruction.opcode()) {
+        0b0110011 => { // R-Type
+            const rtype = RType{
+                .rd = instruction.rd(),
+                .rs1 = instruction.rs1(),
+                .rs2 = instruction.rs2(),
+            };
+            switch (instruction.funct3()) {
+                0b000 => {
+                    switch (instruction.funct7()) {
+                        0b0000000 => {
+                            return DecodedInstruction{ .ADD = rtype };
+                        },
+                        0b0100000 => {
+                            return DecodedInstruction{ .SUB = rtype };
+                        },
+                        else => return error.UnknownInstruction,
+                    }
+                },
+                0b001 => {
+                    return DecodedInstruction{ .SLL = rtype };
+                },
+                0b010 => {
+                    return DecodedInstruction{ .SLT = rtype };
+                },
+                0b011 => {
+                    return DecodedInstruction{ .SLTU = rtype };
+                },
+                0b100 => {
+                    return DecodedInstruction{ .XOR = rtype };
+                },
+                0b101 => {
+                    switch (instruction.funct7()) {
+                        0b0000000 => {
+                            return DecodedInstruction{ .SRL = rtype };
+                        },
+                        0b0100000 => {
+                            return DecodedInstruction{ .SRA = rtype };
+                        },
+                        else => return error.UnknownInstruction,
+                    }
+                },
+                0b110 => {
+                    return DecodedInstruction{ .OR = rtype };
+                },
+                0b111 => {
+                    return DecodedInstruction{ .AND = rtype };
+                },
+            }
+        },
+        0b0010011 => { // I-Type
+            const iType = IType{
+                .rd = instruction.rd(),
+                .rs1 = instruction.rs1(),
+                .imm = instruction.immIType(),
+            };
+
+            switch (instruction.funct3()) {
+                0b000 => {
+                    return DecodedInstruction{ .ADDI = iType };
+                },
+                0b001 => {
+                    return DecodedInstruction{ .SLLI = iType };
+                },
+                0b010 => {
+                    return DecodedInstruction{ .SLTI = iType };
+                },
+                0b011 => {
+                    return DecodedInstruction{ .SLTIU = iType };
+                },
+                0b100 => {
+                    return DecodedInstruction{ .XORI = iType };
+                },
+                0b101 => {
+                    return DecodedInstruction{ .SRLI = iType };
+                },
+                0b110 => {
+                    return DecodedInstruction{ .ORI = iType };
+                },
+                0b111 => {
+                    return DecodedInstruction{ .ANDI = iType };
+                },
+            }
+        },
+        0b0000011 => { // I-Type
+            const iType = IType{
+                .rd = instruction.rd(),
+                .rs1 = instruction.rs1(),
+                .imm = instruction.immIType(),
+            };
+
+            switch (instruction.funct3()) {
+                0b000 => { // LB
+                    return DecodedInstruction{ .LB = iType };
+                },
+                0b001 => { // LH
+                    return DecodedInstruction{ .LH = iType };
+                },
+                0b010 => { // LW
+                    return DecodedInstruction{ .LW = iType };
+                },
+                0b100 => { // LBU
+                    return DecodedInstruction{ .LBU = iType };
+                },
+                0b101 => { // LHU
+                    return DecodedInstruction{ .LHU = iType };
+                },
+                else => return error.UnknownInstruction,
+            }
+        },
+        0b1100111 => { // I-Type
+            switch (instruction.funct3()) {
+                0b000 => {
+                    return DecodedInstruction{
+                        .JALR = IType{
+                            .rd = instruction.rd(),
+                            .rs1 = instruction.rs1(),
+                            .imm = instruction.immIType(),
+                        },
+                    };
+                },
+            }
+        },
+        0b0100011 => { // S-Type
+            const sType = SType{
+                .rs1 = instruction.rs1(),
+                .rs2 = instruction.rs2(),
+                .imm = instruction.immSType(),
+            };
+            switch (instruction.funct3()) {
+                0b000 => { // SB
+                    return DecodedInstruction{ .SB = sType };
+                },
+                0b001 => { // SH
+                    return DecodedInstruction{ .SH = sType };
+                },
+                0b010 => { // SW
+                    return DecodedInstruction{ .SW = sType };
+                },
+                else => return error.UnknownInstruction,
+            }
+        },
+        0b1100011 => { // B-Type
+            const bType = BType{
+                .rs1 = instruction.rs1(),
+                .rs2 = instruction.rs2(),
+                .imm = instruction.immBType(),
+            };
+            switch (instruction.funct3()) {
+                0b000 => { // BEQ
+                    return DecodedInstruction{ .BEQ = bType };
+                },
+                0b001 => { // BNE
+                    return DecodedInstruction{ .BNE = bType };
+                },
+                0b100 => { // BLT
+                    return DecodedInstruction{ .BLT = bType };
+                },
+                0b101 => { // BGE
+                    return DecodedInstruction{ .BGE = bType };
+                },
+                0b110 => { // BLTU
+                    return DecodedInstruction{ .BLTU = bType };
+                },
+                0b111 => { // BGEU
+                    return DecodedInstruction{ .BGEU = bType };
+                },
+                else => return error.UnknownInstruction,
+            }
+        },
+        0b0110111 => { // U-Type
+            return DecodedInstruction{
+                .LUI = UType{
+                    .rd = instruction.rd(),
+                    .imm = instruction.immUType(),
+                },
+            };
+        },
+        0b0010111 => { // U-Type
+            return DecodedInstruction{
+                .AUIPC = UType{
+                    .rd = instruction.rd(),
+                    .imm = instruction.immUType(),
+                },
+            };
+        },
+        0b1101111 => { // J-Type
+            return DecodedInstruction{
+                .JAL = JType{
+                    .rd = instruction.rd(),
+                    .imm = instruction.immJType(),
+                },
+            };
+        },
+        0b1110011 => { // System
+            switch (instruction.funct3()) {
+                0 => {
+                    switch (instruction.funct12()) {
+                        0x000 => {
+                            return DecodedInstruction{ .ECALL = {} };
+                        },
+                        0x001 => {
+                            return DecodedInstruction{ .EBREAK = {} };
+                        },
+                        0x102 => {
+                            return DecodedInstruction{ .SRET = {} };
+                        },
+                        0x105 => {
+                            return DecodedInstruction{ .WFI = {} };
+                        },
+                    }
+                },
+                1 => {
+                    return DecodedInstruction{
+                        .CSRRW = SysType{
+                            .rd = instruction.rd(),
+                            .rs1 = instruction.rs1(),
+                            .csr = instruction.csr(),
+                        },
+                    };
+                },
+                2 => {
+                    return DecodedInstruction{
+                        .CSRRS = SysType{
+                            .rd = instruction.rd(),
+                            .rs1 = instruction.rs1(),
+                            .csr = instruction.csr(),
+                        },
+                    };
+                },
+                3 => {
+                    return DecodedInstruction{
+                        .CSRRC = SysType{
+                            .rd = instruction.rd(),
+                            .rs1 = instruction.rs1(),
+                            .csr = instruction.csr(),
+                        },
+                    };
+                },
+                5 => {
+                    return DecodedInstruction{
+                        .CSRRWI = SysImmType{
+                            .rd = instruction.rd(),
+                            .rs1 = instruction.rs1(),
+                            .csr = instruction.csr(),
+                            .imm = 0, // TODO
+                        },
+                    };
+                },
+                6 => {
+                    return DecodedInstruction{
+                        .CSRRSI = SysType{
+                            .rd = instruction.rd(),
+                            .rs1 = instruction.rs1(),
+                            .csr = instruction.csr(),
+                            .imm = 0, // TODO
+                        },
+                    };
+                },
+                7 => {
+                    return DecodedInstruction{
+                        .CSRRCI = SysType{
+                            .rd = instruction.rd(),
+                            .rs1 = instruction.rs1(),
+                            .csr = instruction.csr(),
+                            .imm = 0, // TODO
+                        },
+                    };
+                },
+                else => return error.UnknownInstruction,
+            }
+        },
+        0b0001111 => { // I-Type
+            switch (instruction.funct3()) {
+                0b000 => {
+                    return DecodedInstruction{ .FENCE = {} };
+                },
+                0b001 => {
+                    return DecodedInstruction{ .FENCEI = {} };
+                },
+                else => return error.UnknownInstruction,
+            }
+        },
+        else => return error.UnknownInstruction,
+    }
+}
--- emulator/src/modules/instruction.zig
+++ emulator/src/modules/instruction.zig
@@ -1,50 +1,55 @@
 const std = @import("std");
 const encode = @import("./encoder.zig");
 
-pub const Instruction = struct {
+pub const RawInstruction = struct {
     value: u32,
 
     // Bits [0:6]
-    pub inline fn opcode(self: Instruction) u7 {
+    pub inline fn opcode(self: RawInstruction) u7 {
         return @truncate(self.value & 0b1111111);
     }
 
     // Bits [7:11]
-    pub inline fn rd(self: Instruction) u5 {
+    pub inline fn rd(self: RawInstruction) u5 {
         return @truncate((self.value >> 7) & 0b11111);
     }
 
     // Bits [12:14]
-    pub inline fn funct3(self: Instruction) u3 {
+    pub inline fn funct3(self: RawInstruction) u3 {
         return @truncate((self.value >> 12) & 0b111);
     }
 
     // Bits [15:19]
-    pub inline fn rs1(self: Instruction) u5 {
+    pub inline fn rs1(self: RawInstruction) u5 {
         return @truncate((self.value >> 15) & 0b11111);
     }
 
     // Bits [20:24]
-    pub inline fn rs2(self: Instruction) u5 {
+    pub inline fn rs2(self: RawInstruction) u5 {
         return @truncate((self.value >> 20) & 0b11111);
     }
 
     // Bits [25:31]
-    pub inline fn funct7(self: Instruction) u7 {
+    pub inline fn funct7(self: RawInstruction) u7 {
         return @truncate((self.value >> 25) & 0b1111111);
     }
 
-    pub inline fn immIType(self: Instruction) i32 {
+    // Bits [20:31]
+    pub inline fn funct12(self: RawInstruction) u12 {
+        return @truncate((self.raw >> 20) & 0xFFF);
+    }
+
+    pub inline fn immIType(self: RawInstruction) i32 {
         return signExtend((self.value >> 20) & 0xFFF, 12);
     }
 
-    pub inline fn immSType(self: Instruction) i32 {
+    pub inline fn immSType(self: RawInstruction) i32 {
         const imm0 = (self.value >> 7) & 0b11111;
         const imm1 = (self.value >> 25) & 0b1111111;
         return signExtend((imm1 << 5) | imm0, 12);
     }
 
-    pub inline fn immBType(self: Instruction) i32 {
+    pub inline fn immBType(self: RawInstruction) i32 {
         const imm0: u32 = (self.value >> 8) & 0b1111; // imm[4:1]
         const imm1: u32 = (self.value & 0b10000000) >> 7; // imm[11]
         const imm2: u32 = (self.value >> 25) & 0b111111; // imm[10:5]
@@ -53,12 +58,12 @@ pub const Instruction = struct {
         return signExtend(imm, 13);
     }
 
-    pub inline fn immUType(self: Instruction) i32 {
+    pub inline fn immUType(self: RawInstruction) i32 {
         const imm: i32 = @bitCast(self.value & 0xFFFFF000);
         return imm >> 12;
     }
 
-    pub inline fn immJType(self: Instruction) i32 {
+    pub inline fn immJType(self: RawInstruction) i32 {
         const imm0 = (self.value >> 12) & 0b11111111; // imm[19:12]
         const imm1 = (self.value >> 20) & 0b1; // imm[11]
         const imm2 = (self.value >> 21) & 0b1111111111; // imm[10:1]
@@ -66,15 +71,15 @@ pub const Instruction = struct {
         return signExtend((imm3 << 20) | (imm0 << 12) | (imm1 << 11) | (imm2 << 1), 21);
     }
 
-    pub inline fn immSystem(self: Instruction) u12 {
+    pub inline fn csr(self: RawInstruction) u12 {
         return @truncate((self.value >> 20) & 0b111111111111);
     }
 
-    pub inline fn pred(self: Instruction) u4 {
+    pub inline fn pred(self: RawInstruction) u4 {
         return @truncate((self.value >> 24) & 0b1111); // Bits [27:24]
     }
 
-    pub inline fn succ(self: Instruction) u4 {
+    pub inline fn succ(self: RawInstruction) u4 {
         return @truncate((self.value >> 20) & 0b1111); // Bits [23:20]
     }
 };
@@ -91,62 +96,62 @@ inline fn signExtend(value: u32, comptime bits: u5) i32 {
 }
 
 test "opcode extraction" {
-    const inst = Instruction{ .value = encode.ADD(1, 2, 3) }; // ADD x1, x2, x3
+    const inst = RawInstruction{ .value = encode.ADD(1, 2, 3) }; // ADD x1, x2, x3
     try std.testing.expectEqual(0b0110011, inst.opcode());
 }
 
 test "rd extraction" {
-    const inst = Instruction{ .value = encode.ADD(10, 2, 3) }; // ADD x10, x2, x3
+    const inst = RawInstruction{ .value = encode.ADD(10, 2, 3) }; // ADD x10, x2, x3
     try std.testing.expectEqual(10, inst.rd()); // rd = x10
 }
 
 test "funct3 extraction" {
-    const inst = Instruction{ .value = encode.ADD(1, 2, 3) }; // ADD x1, x2, x3
+    const inst = RawInstruction{ .value = encode.ADD(1, 2, 3) }; // ADD x1, x2, x3
     try std.testing.expectEqual(0b000, inst.funct3());
 }
 
 test "rs1 extraction" {
-    const inst = Instruction{ .value = encode.ADD(1, 11, 3) }; // ADD x1, x11, x3
+    const inst = RawInstruction{ .value = encode.ADD(1, 11, 3) }; // ADD x1, x11, x3
     try std.testing.expectEqual(11, inst.rs1()); // rs1 = x11
 }
 
 test "rs2 extraction" {
-    const inst = Instruction{ .value = encode.ADD(1, 2, 12) }; // ADD x1, x2, x12
+    const inst = RawInstruction{ .value = encode.ADD(1, 2, 12) }; // ADD x1, x2, x12
     try std.testing.expectEqual(12, inst.rs2()); // rs2 = x12
 }
 
 test "funct7 extraction" {
-    const inst = Instruction{ .value = encode.SUB(1, 2, 3) }; // SUB x1, x2, x3
+    const inst = RawInstruction{ .value = encode.SUB(1, 2, 3) }; // SUB x1, x2, x3
     try std.testing.expectEqual(0b0100000, inst.funct7());
 }
 
 test "I-type immediate extraction" {
-    const inst = Instruction{ .value = encode.ADDI(5, 3, 4) }; // ADDI x5, x3, 4
+    const inst = RawInstruction{ .value = encode.ADDI(5, 3, 4) }; // ADDI x5, x3, 4
     try std.testing.expectEqual(4, inst.immIType());
 }
 
 test "S-type immediate extraction" {
-    const inst = Instruction{ .value = encode.SW(3, 20, 4) }; // SW x3, 20(x4)
+    const inst = RawInstruction{ .value = encode.SW(3, 20, 4) }; // SW x3, 20(x4)
     try std.testing.expectEqual(20, inst.immSType());
 }
 
 test "B-type immediate extraction" {
-    const inst = Instruction{ .value = encode.BEQ(2, 3, -8) }; // BEQ x2, x3, -8
+    const inst = RawInstruction{ .value = encode.BEQ(2, 3, -8) }; // BEQ x2, x3, -8
     try std.testing.expectEqual(-8, inst.immBType());
 }
 
 test "U-type immediate extraction" {
-    const inst = Instruction{ .value = encode.LUI(7, 0x12345) }; // LUI x7, 0x12345
+    const inst = RawInstruction{ .value = encode.LUI(7, 0x12345) }; // LUI x7, 0x12345
     try std.testing.expectEqual(0x12345, inst.immUType());
 }
 
 test "J-type immediate extraction" {
-    const inst = Instruction{ .value = encode.JAL(8, 0x1000) }; // JAL x8, 0x1000
+    const inst = RawInstruction{ .value = encode.JAL(8, 0x1000) }; // JAL x8, 0x1000
     try std.testing.expectEqual(0x1000, inst.immJType());
 }
 
 test "System immediate extraction" {
-    const inst = Instruction{ .value = encode.CSRRC(4, 5, 0x300) }; // CSRRC x4, x5, CSR 0x300
+    const inst = RawInstruction{ .value = encode.CSRRC(4, 5, 0x300) }; // CSRRC x4, x5, CSR 0x300
     try std.testing.expectEqual(0x300, inst.immSystem());
 }
 
