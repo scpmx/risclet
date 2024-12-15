fb5159c600825c91929deecb2683061503e161f2
Stephen <13601394+scpmx@users.noreply.github.com>
2024-12-15 11:40:57 -0500

    Fix issue with csrs

--- emulator/src/modules/csrs.zig
+++ emulator/src/modules/csrs.zig
@@ -1,19 +1,3 @@
-const SStatus = packed struct {
-    SIE: u1 = 0, // Supervisor Interrupt Enable
-    reserved0: u3 = 0, // Reserved (bits 2-4)
-    SPIE: u1 = 0, // Supervisor Previous Interrupt Enable
-    reserved1: u2 = 0, // Reserved (bits 6-7)
-    SPP: u1 = 0, // Supervisor Previous Privilege
-    reserved2: u9 = 0, // Reserved (bits 9-17)
-    UBE: u1 = 0, // User-mode Endianness
-    SUM: u1 = 0, // Supervisor Memory Access
-    MXR: u1 = 0, // Make Executable Readable
-    reserved3: u4 = 0, // Reserved (bits 20-23)
-    FS: u2 = 0, // Floating-Point Status
-    XS: u2 = 0, // Extension Status
-    reserved4: u6 = 0, // Reserved (bits 26-31)
-};
-
 pub const Csrs = struct {
 
     // 0x100
@@ -56,7 +40,7 @@ pub const Csrs = struct {
     // Supervisor Address Translation and Protection
     satp: u32 = 0,
 
-    fn getPtr(self: Csrs, addr: u12) !*u32 {
+    fn getPtr(self: *Csrs, addr: u12) !*u32 {
         return switch (addr) {
             0x100 => &self.sstatus,
             0x104 => &self.sie,
@@ -72,21 +56,21 @@ pub const Csrs = struct {
         };
     }
 
-    pub fn readWrite(self: Csrs, addr: u12, val: u32) !u32 {
+    pub fn readWrite(self: *Csrs, addr: u12, val: u32) !u32 {
         const ptr = try self.getPtr(addr);
         const oldValue = ptr.*;
         ptr.* = val;
         return oldValue;
     }
 
-    pub fn readSet(self: Csrs, addr: u12, val: u32) !u32 {
+    pub fn readSet(self: *Csrs, addr: u12, val: u32) !u32 {
         const ptr = try self.getPtr(addr);
         const oldValue = ptr.*;
         ptr.* |= val;
         return oldValue;
     }
 
-    pub fn readClear(self: Csrs, addr: u12, val: u32) !u32 {
+    pub fn readClear(self: *Csrs, addr: u12, val: u32) !u32 {
         const ptr = try self.getPtr(addr);
         const oldValue = ptr.*;
         ptr.* &= ~val;
